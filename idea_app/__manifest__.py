f2b691bf041e52e39531d77d9667571b297a4461
Fakhri W N <fakhri.waliyyuddin.tif16@polban.ac.id>
2018-03-31 12:08:00 +0700

    Module Idea App, tampilan

---
+++ idea_app/__manifest__.py
@@ -0,0 +1,8 @@
+{
+	'name': 'Ideas',
+	'description': 'Create Vote Our Ideas.',
+	'author': 'Fachri Hammad F.P.',
+	'depends': ['base'],
+	'application': True,
+	'data': [ 'views/idea_view.xml','views/idea_menu.xml',],
+}
