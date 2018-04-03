2edff7abc7cab4427fb41a4dd5d3666c06cd2a4a
Nino <ninomaryono38@gmail.com>
2018-04-03 08:34:24 +0700

    hide button approve&reject for employee + add access for employee

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,7 @@
 	'author': 'Fakhri Waliyyuddin Nugraha',
 	'depends': ['base','mail','hr'],
 	'data': [
+	'security/ir.model.access.csv',
 	'views/vote_menu.xml',
 	'views/vote_view.xml',
 	'views/idea_type_view.xml'
