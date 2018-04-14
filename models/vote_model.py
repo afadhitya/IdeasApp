a9781c64fe1edcd3083b060ebb17136d37e796df
Fakhri W N <fakhri.waliyyuddin.tif16@polban.ac.id>
2018-04-14 23:14:04 +0700

    Penambahan Vitur Vote

--- models/vote_model.py
+++ models/vote_model.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-#
 from odoo import models, fields, api
+from datetime import datetime
+from datetime import timedelta
+import logging	
+
 class VoteApp(models.Model):
 	_name = 'vote.app'
 	_description = 'Vote Application'
@@ -7,13 +11,13 @@ class VoteApp(models.Model):
 
 	name = fields.Char('Title', required=True)
 	employee_ids = fields.Many2one('hr.employee', string='Employee', track_visibility='onchange')
-	date_create = fields.Date('Create Date')
+	date_create = fields.Date(string='Create Date', default=datetime.now())
 	company_id = fields.Many2one('res.company', 'Company')
 	department_id = fields.Many2one('hr.department', string='Department')
-	date_deadline = fields.Date('Deadline', required=True)
+	date_deadline = fields.Date(string='Deadline',default=datetime.now() + timedelta(days=14), required=True)
 	idea_id = fields.Many2one('idea.type',string='Idea Type')
 	description = fields.Text('Notes',required=True)
-	vote_list = fields.Many2many('wizard.vote')
+	vote_list = fields.One2many('wizard.vote','ideas_id',string='Employee Votes')
 	state = fields.Selection([
         ('new', 'New'),
         ('waiting', 'Waiting for Approval'),
@@ -23,27 +27,34 @@ class VoteApp(models.Model):
         ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', default='new')
 
 
-	@api.multi
+	@api.one
 	def do_approve(self):
 		self.write({'state': 'approved'})
 		return True
 
-		
-	@api.multi
+	@api.one
 	def do_reject(self):
 		self.write({'state': 'reject'})
 		return True
 
-	@api.multi
+	@api.one
 	def do_post_idea(self):
 		self.write({'state': 'waiting'})
 		return True
 
 	@api.multi
-	def do_give_vote(self):
-		return True
-
-	@api.multi
+	def vote_wizard(self):
+		_logger = logging.getLogger(__name__)
+		_logger.debug('ID:'+ str(self.id))
+		return {
+			'view_type': 'form',
+			'view_mode': 'form',
+			'res_model': 'wizard.vote',
+			'target': 'new',
+			'type': 'ir.actions.act_window',
+			'context': {'current_id': self.id},
+		}
+	@api.one
 	def do_closed(self):
 		self.write({'state': 'closed'})
 		return True
--- wizard/wizard_give_vote.py
+++ wizard/wizard_give_vote.py
@@ -1,4 +1,5 @@
 from odoo import tools, api, fields, models
+import logging
 
 AVAILABLE_PRIORITIES = [
 	('1', 'Very Low'),
@@ -12,6 +13,13 @@ AVAILABLE_PRIORITIES = [
 class open_wizard_vote(models.TransientModel):
 	_name = 'wizard.vote'
 
+	@api.model
+	def _current_id(self):
+		_logger = logging.getLogger(__name__)
+		_logger.debug('CONTEXT ID:'+ str(self.id))
+		return self.env['vote.app'].browse(self._context.get('current_id', 1))
+
+	ideas_id = fields.Many2one('vote.app', string="Ideas", required=True, default=_current_id, readonly=True)
 	priority = fields.Selection(AVAILABLE_PRIORITIES, string='Rating')
 	comment = fields.Text('Your Comment', required=True)
 
