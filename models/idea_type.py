4e9341ea9727143a5a6fa78b938537246072e94c
Fakhri W N <fakhri.waliyyuddin.tif16@polban.ac.id>
2018-04-15 19:06:50 +0700

    Compute Total Idea Type

--- models/idea_type.py
+++ models/idea_type.py
@@ -9,5 +9,13 @@ class IdeaType(models.Model):
 	name = fields.Char('Name', required=True)
 	min_vote = fields.Integer(string='Minimum Vote', readonly=True)
 	max_vote = fields.Integer(string='Maximum Vote', readonly=True)
-	total_ideas = fields.Integer(string='Total Ideas', readonly=True)
+	total_ideas = fields.Integer(compute='_compute_total_type_idea', string='Total Ideas', readonly=True)
 	department_list = fields.Many2many('hr.department')
+
+	idea_type = fields.One2many(comodel_name='vote.app', inverse_name='idea_id', string='Idea Type')
+
+	@api.multi
+	def _compute_total_type_idea(self):
+		for tipe in self:
+			tipe.total_ideas = len(tipe.idea_type)
+			
