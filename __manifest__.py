fbf1066c97ecbb89055f9600ec4bfe6d6e822009
Achmad Fadhitya <afadhitya@gmail.com>
2018-04-15 23:54:07 +0700

    Add print button and print report

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,8 @@
 		'views/vote_menu.xml',
 		'views/vote_view.xml',
 		'views/idea_type_view.xml',
-		'reports/report_vote.xml'
+		'reports/report_vote.xml',
+		'reports/report_vote_template.xml'
 	],
 	'application': True,
 }
