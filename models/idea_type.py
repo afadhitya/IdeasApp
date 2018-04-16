112ce32febe01fb6ba2e23f4a3688ce802915d21
Fakhri W N <fakhri.waliyyuddin.tif16@polban.ac.id>
2018-04-16 19:09:23 +0700

    Update Max Min Vote

--- models/idea_type.py
+++ models/idea_type.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+import logging
 
 class IdeaType(models.Model):
 	_name = 'idea.type'
@@ -7,8 +8,8 @@ class IdeaType(models.Model):
 	_order = "name"
 
 	name = fields.Char('Name', required=True)
-	min_vote = fields.Integer(string='Minimum Vote', readonly=True)
-	max_vote = fields.Integer(string='Maximum Vote', readonly=True)
+	min_vote = fields.Integer(string='Minimum Vote',compute='_vote_count_max_min',readonly=True)
+	max_vote = fields.Integer(string='Maximum Vote',compute='_vote_count_max_min',readonly=True)
 	total_ideas = fields.Integer(compute='_compute_total_type_idea', string='Total Ideas', readonly=True)
 	
 	department_list = fields.Many2many('hr.department')
@@ -18,4 +19,23 @@ class IdeaType(models.Model):
 	def _compute_total_type_idea(self):
 		for tipe in self:
 			tipe.total_ideas = len(tipe.idea_type)
-			
+
+
+	@api.one
+	def _vote_count_max_min(self):
+		_logger = logging.getLogger(__name__)
+		ideas = self.env['vote.app'].search([('idea_id','=', self.id)])
+		min = 0
+		max = 0
+		i=0
+		for idea in ideas:
+			num_vote = len(self.env['wizard.vote'].search([('ideas_id','=', idea.id)]))
+			if i == 0 or num_vote<min:
+				min = num_vote
+			if i == 0 or num_vote>max:
+				max = num_vote
+			_logger.debug("MASUK: " + str(num_vote))
+			i = i + 1
+
+		self.min_vote = min
+		self.max_vote = max
